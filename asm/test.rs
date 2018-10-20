86771b85e37a8911fca036670bb42236ce398843
David Spickett <spickettdavid@googlemail.com>
2018-10-20 17:21:22 +0100

    Cleanup whitespace and uneeded slashes in multi line strings.

--- asm/test.rs
+++ asm/test.rs
@@ -74,9 +74,9 @@ mod test {
     #[test]
     fn either_case_mnemonics() {
         let asm_tests = [
-            ("cls", "CLS"),
-            ("jp 0x123", "JP 0x123"),
-            ("add V0, V1", "ADD V0, V1"),
+            ("cls",          "CLS"),
+            ("jp 0x123",     "JP 0x123"),
+            ("add V0, V1",   "ADD V0, V1"),
             (".WORD 0x1234", ".word 0x1234"),
         ];
         assert_asm_roundtrip(&asm_tests);
@@ -85,20 +85,20 @@ mod test {
     #[test]
     fn either_case_and_hex_v_regs() {
         let asm_tests = [
-            ("ADD V0, V1", "ADD V0, V1"),
-            ("ADD v0, V1", "ADD V0, V1"),
-            ("ADD V0, v1", "ADD V0, V1"),
+            ("ADD V0, V1",       "ADD V0, V1"),
+            ("ADD v0, V1",       "ADD V0, V1"),
+            ("ADD V0, v1",       "ADD V0, V1"),
             //A bit silly but they work so lets check them
             ("ADD V0000000, v1", "ADD V0, V1"),
-            ("ADD V00F, v1", "ADD V15, V1"),
+            ("ADD V00F, v1",     "ADD V15, V1"),
 
-            ("ADD VA, V10", "ADD V10, V10"),
-            ("ADD V02, VF", "ADD V2, V15"),
+            ("ADD VA, V10",      "ADD V10, V10"),
+            ("ADD V02, VF",      "ADD V2, V15"),
 
             // This instr always has V0, but it should verify it the same way
-            ("JP V0, 0x123", "JP V0, 0x123"),
-            ("JP v0, 0x456", "JP V0, 0x456"),
-            ("JP V000, 0x789", "JP V0, 0x789"),
+            ("JP V0, 0x123",     "JP V0, 0x123"),
+            ("JP v0, 0x456",     "JP V0, 0x456"),
+            ("JP V000, 0x789",   "JP V0, 0x789"),
         ];
         assert_asm_roundtrip(&asm_tests);
     }
@@ -110,21 +110,21 @@ mod test {
             // functions and just check one of each
 
             // 12 bit addresses
-            ("SYS 0x123", "SYS 0x123"),
-            ("SYS 0x23", "SYS 0x023"),
+            ("SYS 0x123",        "SYS 0x123"),
+            ("SYS 0x23",         "SYS 0x023"),
             ("SYS 0x0000000023", "SYS 0x023"),
             ("SYS 0x0000000323", "SYS 0x323"),
 
             // Bytes
-            ("RND V0, 0x12", "RND V0, 0x12"),
-            ("RND V0, 0x2", "RND V0, 0x02"),
+            ("RND V0, 0x12",       "RND V0, 0x12"),
+            ("RND V0, 0x2",        "RND V0, 0x02"),
             ("RND V0, 0x00000002", "RND V0, 0x02"),
 
             // 16 bit values
-            (".word 0x1234", ".word 0x1234"),
-            (".word 0x234", ".word 0x0234"),
-            (".word 0x34", ".word 0x0034"),
-            (".word 0x4", ".word 0x0004"),
+            (".word 0x1234",     ".word 0x1234"),
+            (".word 0x234",      ".word 0x0234"),
+            (".word 0x34",       ".word 0x0034"),
+            (".word 0x4",        ".word 0x0004"),
             (".word 0x00000004", ".word 0x0004"),
             (".word 0x00001234", ".word 0x1234"),
 
@@ -142,23 +142,25 @@ mod test {
 
     #[test]
     fn test_blank_lines_ignored() {
-        let asm = "\
-        \t\n\
+        let asm = "
+        JP t
+        \t
+        t:
         DRW V0, V1, 6
              \n\
         ADD V0, 0x34".to_string();
-        let expected: [u16; 2] = [0xD016, 0x7034];
+        let expected: Vec<u16> = vec![0x1202, 0xD016, 0x7034];
 
         assert_asm_bitpatterns(&asm, &expected);
     }
 
     #[test]
     fn test_single_line_comments_ignored() {
-        let asm = "\
-        // This is a single line comment\n\
-        DRW V5, V2, 7\n\
-        LD F, V9 // This is one the end of a line\n\
-        LD ST, V2// This one has no space after the arg\n\
+        let asm = "
+        // This is a single line comment
+        DRW V5, V2, 7
+        LD F, V9 // This is one the end of a line
+        LD ST, V2// This one has no space after the arg
         CLS//This one LD ST, V7 includes an instr".to_string();
         let expected: Vec<u16> = vec![0xD527, 0xF929, 0xF218, 0x00E0];
 
@@ -167,14 +169,14 @@ mod test {
 
     #[test]
     fn branch_back_to_label() {
-        let asm = "\
-        SYS 0x000\n\
-        // Comment lines don't increment address\n\
-        foo:\n\
-        SYS 0x111\n\
+        let asm = "
+            SYS 0x000
+            // Comment lines don't increment address
+        foo:
+            SYS 0x111
         bar:: // Should be refered to as 'bar:' just fine
-        JP foo\n\
-        JP bar:".to_string();
+            JP foo
+            JP bar:".to_string();
         let expected: Vec<u16> = vec![0x0000, 0x0111, 0x1202, 0x1204];
 
         assert_asm_bitpatterns(&asm, &expected);
@@ -183,14 +185,14 @@ mod test {
     #[test]
     fn branch_forward_to_label() {
         let asm = "\
-        JP start
-        CALL start
-        SYS start
-        JP V0, start
-        LD I, start
+            JP start
+            CALL start
+            SYS start
+            JP V0, start
+            LD I, start
         start:
         self:
-        JP self".to_string();
+            JP self".to_string();
         let expected: Vec<u16> = vec![0x120a, 0x220a, 0x020a, 0xB20a, 0xA20a, 0x120a];
 
         assert_asm_bitpatterns(&asm, &expected);
@@ -199,25 +201,25 @@ mod test {
     #[test]
     #[should_panic(expected="Could not resolve symbol \"aardvark\"")]
     fn unresolved_symbol_panics() {
-        let asm = "\
-        JP next\n\
-        next:\n\
-        CALL next\n\
-        JP aardvark".to_string();
+        let asm = "
+            JP next
+        next:
+            CALL next
+            JP aardvark".to_string();
 
         parse_asm(&asm);
     }
 
     #[test]
     fn symbol_capable_instructions() {
-        let asm ="\
-        SYS 0x000\n\
-        SYS 0x000\n\
-        target:\n\
-        SYS target\n\
-        JP target\n\
-        CALL target\n\
-        LD I, target".to_string();
+        let asm ="
+            SYS 0x000
+            SYS 0x000
+        target:
+            SYS target
+            JP target
+            CALL target
+            LD I, target".to_string();
         let expected: Vec<u16> = vec![0x0000, 0x0000, 0x0204, 0x1204, 0x2204, 0xA204];
 
         assert_asm_bitpatterns(&asm, &expected);
@@ -295,13 +297,13 @@ mod test {
 
     #[test]
     fn word_directive() {
-        let asm = "\
+        let asm = "
             CALL end // Jump to check that .word does increment the address
-            .word 0x1234\n\
-            ADD V0, V1\n\
-            .word 0x0000\n\
-            .word 0x2111 // Value can overlap real instructions\n\
-        end:\n\
+            .word 0x1234
+            ADD V0, V1
+            .word 0x0000
+            .word 0x2111 // Value can overlap real instructions
+        end:
             ADD V2, V3".to_string();
         let expected: Vec<u16> = vec![0x220A, 0x1234, 0x8014, 0x0000, 0x2111, 0x8234];
         assert_asm_bitpatterns(&asm, &expected);
